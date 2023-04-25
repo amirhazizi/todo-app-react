6c1d131e79552d64b0feaee6c4c3521639d55b44
amir <amirazizihosein@gmail.com>
2023-04-25 21:30:34 +0430

    setup project

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      // ## Screen sizes
+      screens: {
+        sm: "480px",
+        md: "768px",
+        lg: "976px",
+        lx: "1440px",
+      },
+    },
+    plugins: [],
+  },
+}
